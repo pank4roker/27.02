d1ce5add50ee79db79bafaf1da113fbe19f93322
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2023-02-26 20:52:16 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //z1
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"array[{ i}]");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //Console.ReadKey();
+            // z 2
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //Random rnd = new Random();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = rnd.Next(-10, 10);
+            //    Console.WriteLine($"array[{i}]= {array[i]}");
+            //}
+            //Console.ReadKey();
+            // z3
+            //int[] array = new int[] { 2, 4, 7, -6, 8, 6, 42, 57 };
+            //int max = array[0]; int index1 = 0;
+            //int min = array[0]; int index2 = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+               
+            //    Console.WriteLine($"array[{i}]= {array[i]}");
+            //}
+            //// 1 yslovie
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    if (array[i] > max)
+            //    {
+            //        max = array[i];
+            //        index1 = i;
+            //    }
+            //}
+            //Console.WriteLine($"max={max};index1= {index1}");
+            //// 2 yslovie
+            //for (int i = 1; i < array.Length; i++)
+            //{
+            //    if (array[i] < min)
+            //    {
+            //        min = array[i];
+            //        index2 = i;
+            //    }
+            //}
+            ////4 yslovie
+            //Console.WriteLine($"min={min};index2={index2}");
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //Console.WriteLine($"Количество элементов между максимумом и минимумом={Math.Abs(index2 - index1) - 1}");
+            ////3 yslovie
+            //int p;
+            //p = array[index1];
+            //array[index1] = array[index2];
+            //array[index2] = p;
+            //for (int i = 0; i <= array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]= {array[i]}");
+            //}
+            //Console.ReadKey();
+            
+
+        }
+    }
+}
